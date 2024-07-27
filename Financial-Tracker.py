fe73339e1305acdc22dc867ada8c3d35f93d816a
Lucas-Ruiz-Carrascosa <lucas.ruizcarrascosa@aalto.fi>
2024-07-27 21:28:00 +0200

    Update Financial-Tracker.py

--- Financial-Tracker.py
+++ Financial-Tracker.py
@@ -11,7 +11,10 @@ with tab1:
     st.subheader("Upload a csv file, you can download an updated version when you have finnished using the app.")
     csv_file = st.file_uploader("This file is generated from this website, and is used to keep track of previous financial info. You can download the updated file from here as well", "csv", key="csv_file")
     if csv_file:
-    
+        #Saving file
+        with open(csv_file.name, "wb") as f:
+            f.write(csv_file.getbuffer())
+        
         st.download_button("Download csv", csv_file, "expenses.csv")
     else:
         st.header("Error")
