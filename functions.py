5fbbd1934d255a39024f8ea1213320f99f0d9655
Lucas-Ruiz-Carrascosa <lucas.ruizcarrascosa@aalto.fi>
2024-07-28 11:38:48 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from datetime import date
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 expense_count = 0
 
@@ -10,24 +10,20 @@ expense_list = []
 
 
 
-def total_expenses(expense_file_path="expenses.csv"):
-    with open(expense_file_path,"r") as f:
-        lines = len(list(f))
-        return lines
+def total_expenses(datafile):
+    return len(datafile)
 
-def save_expense_to_file(name, amount, category, expense_file_path = "expenses.csv"):
-    with open(expense_file_path, "a") as f:
-        f.write(f"{date.today().day},{date.today().month}/{date.today().year}, {name}, {amount}, {category}, {date.today().year}\n")
+
+    
         
 
-def sumarize_expenses(budget, expense_file_path="expenses.csv"):
+def sumarize_expenses(budget, datafile):
     
     expenses = []
-    with open(expense_file_path, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            day, month_year, expense_name, expense_amount, expense_category, year = line.strip().split(",")
-            expenses.append([day, month_year, expense_name, expense_amount, expense_category, year])
+    
+    for index, rows in datafile.iterrows():
+        my_list =[rows[0], rows[1], rows[2], rows[3], rows[4], rows[5]]
+        expenses.append(my_list)
 
     amount_by_category = {}
     for expense in expenses:
@@ -48,8 +44,7 @@ def sumarize_expenses(budget, expense_file_path="expenses.csv"):
         else:
             amount_by_month[key] = float(expense[3])
 
-    #for key, amount in amount_by_month.items():
-        #st.write(f"  {key}: {amount:.2f}")
+    
     
     amount_by_year = {}
     for expense in expenses:
@@ -66,10 +61,10 @@ def sumarize_expenses(budget, expense_file_path="expenses.csv"):
         df = pd.DataFrame(list(amount_by_month.items()), columns=["Month/Year", "Total spending"])
         st.dataframe(df)
         st.line_chart(df, x="Month/Year", y="Total spending")
-        df2 = pd.read_csv(expense_file_path, usecols=[5,3], header=None, names=["Total Spending", "Year"], dtype={"Year": str})
+
         
-        st.bar_chart(df2, x="Year", y="Total Spending")
         df3 = pd.DataFrame(list(amount_by_year.items()), columns= ["Year", "Total spending"])
+        st.bar_chart(df3, x="Year", y="Total spending")
         st.dataframe(df3)
         
         
